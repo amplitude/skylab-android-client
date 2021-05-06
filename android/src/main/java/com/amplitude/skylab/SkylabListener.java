63ca437be2d6c6f6319707b3d82da3b529793026
Brian Giori <brian.giori@amplitude.com>
2021-05-06 12:57:58 -0700

    feat: Clean up enrollment ID; Rename to Assignment (#5)
    BREAKING CHANGE

--- android/src/main/java/com/amplitude/skylab/SkylabListener.java
+++ android/src/main/java/com/amplitude/skylab/SkylabListener.java
@@ -3,7 +3,7 @@ package com.amplitude.skylab;
 import java.util.Map;
 
 /**
- * Classes can implement this interface to handle changes in enrollment.
+ * Classes can implement this interface to handle changes in assigned variants for a user.
  * The {@link #onVariantsChanged(SkylabUser, Map)} method is called when a
  * {@link SkylabClient} fetches variant values that are different from previously fetched
  * values.
